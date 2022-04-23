d9f86feca3d2be03280902926d945dbd055ff029
Aoki Hiroshige <aoki.hiroshige.18@shizuoka.ac.jp>
2022-04-24 03:05:36 +0900

    Revert "Revert "first commit""
    This reverts commit 7c661d828cbd663649365873d2a534af3f40c0db.

---
+++ townwork/partime.jl
@@ -0,0 +1,4 @@
+{"tw_sa99129": "https://townwork.net/shizuoka/ct_ma60001/tw_sa99129/"}
+{"sc_ss9912904": "https://townwork.net/shizuoka/ct_ma60001/tw_sa99129/sc_ss9912904/"}
+{"sc_ss9912903": "https://townwork.net/shizuoka/ct_ma60001/tw_sa99129/sc_ss9912903/"}
+{"sc_ss9912902": "https://townwork.net/shizuoka/ct_ma60001/tw_sa99129/sc_ss9912902/"}
