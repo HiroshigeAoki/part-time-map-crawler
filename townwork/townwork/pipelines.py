ae6089d6ea2d50e14e578b221a8a52827534889f
HiroshigeAoki <a109k9i8@gmail.com>
2022-05-29 23:08:38 +0900

    fixed

--- townwork/townwork/pipelines.py
+++ townwork/townwork/pipelines.py
@@ -8,19 +8,14 @@
 from scrapy.exceptions import DropItem
 from pymongo import MongoClient
 
-class ValidationPipline:
-    def validate_item(self, item, spider):
+class ValidationPipline: #TODO:後で書く。
+    def process_item(self, item, spider):
         if not item['']:
             raise DropItem('Missing title')
-    def drop_duplication(self, item, spider):
-        if item: #TODO:後で書く。
+        if item:
             raise DropItem('Drop duplication')
         return item
-    def calc_lat_loc(self, item, spider):
-        if item['lat'] is None and item['loc'] is None:
-            item['lat'] = '' #TODO: 後で
-            item['loc'] = ''
-        return item
+        
 
 class MongoPipeline:
     def open_spider(self, spider):
